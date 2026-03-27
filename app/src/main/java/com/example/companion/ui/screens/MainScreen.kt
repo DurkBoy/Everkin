ea9b95ae68728735d0dc6f0449d6a2a7f1bf0e5c
DurkBoy <durkeedavid02@gmail.com>
2026-03-27 01:35:39 -0400

    Opt in to experimental Material3 API in MainScreen

--- app/src/main/java/com/example/companion/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/companion/ui/screens/MainScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +16,7 @@ import com.example.companion.ui.components.CreatureImage
 import com.example.companion.ui.components.StatsPanel
 import com.example.companion.viewmodel.MainViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     viewModel: MainViewModel,
